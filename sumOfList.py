c0b636fd1c6a4ef315b6f99fc20a0763641a18fc
Girish Siddananjappa Gangappa <gangappa.girish@gmail.com>
2017-02-27 20:14:17 -0800

    Create sumOfList.py

---
+++ sumOfList.py
@@ -0,0 +1,8 @@
+https://www.hackerrank.com/challenges/a-very-big-sum?h_r=next-challenge&h_v=zen
+
+#!/bin/python3
+
+import sys
+n=int(input())
+arr=[int(curarr) for curarr in input().strip().split(" ")]
+print(sum(arr))
